91a5a87856b9da2240ece1425e27d5903b2e4827
shizhiang <shizhiang-hj@huafang.com>
2023-01-07 20:19:24 +0800

    Create main project and serveral submodules

---
+++ gulimall-member/src/test/java/com/jacob/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/jacob/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
